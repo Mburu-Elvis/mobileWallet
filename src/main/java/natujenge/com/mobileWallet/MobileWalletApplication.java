ed90f0a2c3a721200cc8ca488628a88c31bb65a6
elvis-dev <mburuelvis21@gmail.com>
2024-07-23 13:32:44 +0300

    project initialization

---
+++ src/main/java/natujenge/com/mobileWallet/MobileWalletApplication.java
@@ -0,0 +1,13 @@
+package natujenge.com.mobileWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileWalletApplication.class, args);
+	}
+
+}
