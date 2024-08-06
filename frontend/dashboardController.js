ec9c091b21d4d4c54e9415f029f64671f7bf7df3
elvis-dev <mburuelvis21@gmail.com>
2024-08-07 00:29:34 +0300

    added deposit logic

--- frontend/dashboardController.js
+++ frontend/dashboardController.js
@@ -25,4 +25,29 @@ app.controller("DashboardController", ["$scope", "$http", "$route", function($sc
 	    $scope.sendForm = {};
         });
     };
+
+    $scope.depositMoney = function() {
+
+            $scope.sendForm.from = "0701888380";
+            $scope.sendForm.to = "0701888380";
+            console.log($scope.sendForm)
+            $http({
+                method: 'POST',
+                headers: {
+                    'ngrok-skip-browser-warning': 'hello',
+                    'Content-Type': 'application/json'
+                },
+                data: $scope.sendForm,
+                url: 'https://stallion-holy-informally.ngrok-free.app/api/v1/payments/deposit',
+            }).then(function(response) {
+                // Success callback
+                console.log('Accounts:', response);
+                $scope.refreshAccounts();
+    	    $scope.sendForm = {};
+            }, function(error) {
+                // Error callback
+                console.error('Error fetching accounts:', error);
+    	    $scope.sendForm = {};
+            });
+        };
 }]);
