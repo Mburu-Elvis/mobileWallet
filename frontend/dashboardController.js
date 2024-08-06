f2abe0f040c1b05e18d96cddf49b6af286800718
MBAI66 <feizmbai900@gmail.com>
2024-08-06 12:49:36 +0300

    add modal class for send money

--- frontend/dashboardController.js
+++ frontend/dashboardController.js
@@ -1,3 +1,26 @@
 app.controller("DashboardController", ["$scope", "$http", "$route", function($scope, $http, $route) {
-
+    $scope.sendForm = {};
+    
+    
+    $scope.sendMoney = function() {
+        $http({
+            method: 'POST',
+            headers: {
+                'ngrok-skip-browser-warning': 'hello',
+                'Content-Type': 'application/json'
+            },
+            data: $scope.sendForm,
+            url: 'https://stallion-holy-informally.ngrok-free.app/api/v1/employees',
+        }).then(function(response) {
+            // Success callback
+            console.log('Accounts:', response);
+            $scope.refreshAccounts();
+	    $scope.sendForm = {};
+        }, function(error) {
+            // Error callback
+            console.error('Error fetching accounts:', error);
+            $scope.refreshAccounts();
+	    $scope.sendForm = {};
+        });
+    };
 }]);
